fb86935016b434c418b8b132d9d0eca560c7c744
hudson-newey <grath@duck.com>
2024-08-23 21:58:11 +1000

    Only echo back file paths

--- src/main.go
+++ src/main.go
@@ -8,12 +8,29 @@ import (
 	"hudson-newey/2rm/src/commands"
 )
 
+func extractFileNames(args []string) []string {
+	fileNames := []string{}
+
+	for _, str := range args {
+		if !strings.HasPrefix(str, "-") {
+			fileNames = append(fileNames, str)
+		}
+	}
+
+	return fileNames
+}
+
 func main() {
-	args := strings.Join(os.Args[1:], " ")
-	args = strings.ReplaceAll(args, " --no-preserve-root", "")
+	originalArguments := os.Args[1:];
 
-	fmt.Println(args)
+	// I remove and modify some arguments from the rm command
+	args := strings.Join(originalArguments, " ")
+	actionedArgs := strings.ReplaceAll(args, " --no-preserve-root", "")
 
-	gitCommand := "rm " + args
+	fileNames := extractFileNames(originalArguments)
+	debugStatement := strings.Join(fileNames, " ")
+	fmt.Println(debugStatement)
+
+	gitCommand := "rm " + actionedArgs
 	commands.Execute(gitCommand)
 }
