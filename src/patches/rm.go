ddd4aec6f99f71e25996714918a053c3147e8f6b
hudson-newey <grath@duck.com>
2024-09-03 23:22:09 +1000

    Preserve backups

--- src/patches/rm.go
+++ src/patches/rm.go
@@ -2,21 +2,33 @@ package patches
 
 import (
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 	"strings"
+	"time"
 
-	"hudson-newey/2rm/src/commands"
 	"hudson-newey/2rm/src/util"
 )
+
+const TRASH_DIR_PERMISSIONS = 0755
 
 func RmPatch(arguments []string) {
 	actionedArgs := removeDangerousArguments(arguments)
 
-	fileNames := extractFileNames(actionedArgs)
-	debugStatement := strings.Join(fileNames, " ")
+	filePaths := extractFilePaths(actionedArgs)
+
+	// TODO: this should probably allow hard deletion of /tmp files
+	createBackups(filePaths)
+
+	// a debug statement is useful for scripts, it provides explicit feedback
+	// and prints exactly what files were backed up / moved to the trash can
+	// after deletion
+	debugStatement := strings.Join(filePaths, " ")
 	fmt.Println(debugStatement)
 
-	command := "rm " + strings.Join(actionedArgs, " ")
-	commands.Execute(command)
+	// command := "rm " + strings.Join(actionedArgs, " ")
+	// commands.Execute(command)
 }
 
 func removeDangerousArguments(arguments []string) []string {
@@ -36,14 +48,90 @@ func removeDangerousArguments(arguments []string) []string {
 	return returnedArguments
 }
 
-func extractFileNames(args []string) []string {
-	fileNames := []string{}
+func extractFilePaths(args []string) []string {
+	filePaths := []string{}
 
 	for _, str := range args {
 		if !strings.HasPrefix(str, "-") {
-			fileNames = append(fileNames, str)
+			filePaths = append(filePaths, str)
 		}
 	}
 
-	return fileNames
+	return filePaths
+}
+
+func relativeToAbsolute(path string) string {
+	absolutePath, err := filepath.Abs(path)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(3)
+	}
+
+	return absolutePath
+}
+
+func backupFileName(path string) string {
+	result := strings.ReplaceAll(path, ".", "-")
+	result = strings.ReplaceAll(result, "/", "_")
+	return result + ".bak"
+}
+
+func createBackups(filePaths []string) {
+	tempDir := "/tmp/2rm/"
+	deletedTimestamp := time.Now().Format(time.RFC3339)
+	backupDirectory := tempDir + deletedTimestamp
+
+	err := os.MkdirAll(backupDirectory, TRASH_DIR_PERMISSIONS)
+	if err != nil {
+		fmt.Println("Failed to create trash can entry in", backupDirectory)
+		fmt.Println("Continue? (y/n)")
+
+		var response string
+		fmt.Scanln(&response)
+
+		// unless the user explicitly states that they want to continue
+		// without a backup, we want to exit
+		// everything other than a "y"/"yes" response will not delete the file
+		if response != "y" && response != "yes" {
+			fmt.Println("Exiting without removing file(s).")
+			os.Exit(1)
+		}
+	}
+
+	for _, path := range filePaths {
+		absoluteSrcPath := relativeToAbsolute(path)
+
+		backupFileName := backupFileName(path)
+		backupLocation := backupDirectory + "/" + backupFileName
+
+		sourceFile, err := os.Open(absoluteSrcPath)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		defer sourceFile.Close()
+
+		destFile, err := os.Create(backupLocation)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+		defer destFile.Close()
+
+		_, err = io.Copy(sourceFile, destFile)
+		if err != nil {
+			fmt.Println(err)
+			fmt.Println()
+			fmt.Println("Failed to copy file for", path)
+			fmt.Println("Continue? (y/n)")
+
+			var response string
+			fmt.Scanln(&response)
+
+			if response != "y" && response != "yes" {
+				fmt.Println("Exiting without removing file(s).")
+				os.Exit(2)
+			}
+		}
+	}
 }
